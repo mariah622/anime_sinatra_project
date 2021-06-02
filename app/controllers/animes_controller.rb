36b76494f11c0f2a6d7201d1a005c82070afc533
Mariah Morales <mjustine622@gmail.com>
2021-06-02 13:39:36 -0400

    third commit

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -2,6 +2,27 @@ class AnimesController < ApplicationController
 
     get '/animes' do
         # @animes = Anime.all
-        erb :'/animes/index'
+        erb :'animes/index'
     end 
+
+    get '/animes/new' do
+        #form to create new movie
+        erb :'animes/new'
+    end
+
+    get '/animes/:id/edit'
+        erb :'animes/edit'
+
+    post '/animes' do 
+    # create new animes
+    end 
+
+    patch '/animes/:id' do
+        # edit a particular anime selectin
+    end 
+
+    delete '/anime/:id' do 
+        #edit a particular anime selection
+    end 
+
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,22 @@ class UsersController < ApplicationController
     end
     
     post '/signup' do 
+        # we must save this user. if user is not save, they should be redicreted. if they did not fill in the proper infr we must redirect back to teh sign in page. after a successful sign in, we then can redirect to '/animes' page. 
     end
 
+    get '/login' do 
+        erb :'users/login'
+    end 
 
-    get '/users' do
-        erb :'users/here'
-    end
+    # post '/login' do 
+        # redirect to '/animes'. it should only redirect if you are loggin in. 
+    # end 
+
+    # post '/logout do'
+    # end 
+
+
+
+
+    
 end 
